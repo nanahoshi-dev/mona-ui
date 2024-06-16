2c847564ce2840475788c0ee4295f7bbb028994b
Phrolova <phrolova@nanahoshi.dev>
2024-06-16 18:40:36 +0300

    feat: context menu for grid rows

--- projects/mona-ui/src/lib/grid/directives/grid-context-menu.directive.spec.ts
+++ projects/mona-ui/src/lib/grid/directives/grid-context-menu.directive.spec.ts
@@ -1,16 +1,8 @@
-import { TestBed } from "@angular/core/testing";
-import { GridService } from "../services/grid.service";
-import { GridContextMenuDirective } from "./grid-context-menu.directive";
+import { GridContextMenuDirective } from './grid-context-menu.directive';
 
-describe("GridContextMenuDirective", () => {
-    let directive: GridContextMenuDirective;
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [GridService]
-        });
-        directive = TestBed.runInInjectionContext(() => new GridContextMenuDirective());
-    });
-    it("should create an instance", () => {
-        expect(directive).toBeTruthy();
-    });
+describe('GridContextMenuDirective', () => {
+  it('should create an instance', () => {
+    const directive = new GridContextMenuDirective();
+    expect(directive).toBeTruthy();
+  });
 });
