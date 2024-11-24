749d3c8e512ca16866aebb8b40a00d6647a8b658
Phrolova <phrolova@nanahoshi.dev>
2024-11-24 15:09:53 +0300

    feat: update to Angular 19
    BREAKING CHANGE: This will update the library to Angular 19.

--- projects/mona-ui/src/lib/layout/splitter/components/splitter-resizer/splitter-resizer.component.spec.ts
+++ projects/mona-ui/src/lib/layout/splitter/components/splitter-resizer/splitter-resizer.component.spec.ts
@@ -1,22 +1,37 @@
+import { Component } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { SplitterPaneComponent } from "../splitter-pane/splitter-pane.component";
+import { SplitterComponent } from "../splitter/splitter.component";
 
 import { SplitterResizerComponent } from "./splitter-resizer.component";
 
+@Component({
+    template: `
+        <mona-splitter>
+            <mona-splitter-pane> Pane 1</mona-splitter-pane>
+            <mona-splitter-pane> Pane 2</mona-splitter-pane>
+        </mona-splitter>
+    `,
+    styles: "",
+    imports: [SplitterComponent, SplitterPaneComponent]
+})
+class TestHostComponent {}
+
 describe("SplitterResizerComponent", () => {
-    let component: SplitterResizerComponent;
-    let fixture: ComponentFixture<SplitterResizerComponent>;
+    let hostComponent: TestHostComponent;
+    let hostFixture: ComponentFixture<TestHostComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [SplitterResizerComponent]
+            imports: [SplitterComponent, SplitterPaneComponent, TestHostComponent]
         }).compileComponents();
 
-        fixture = TestBed.createComponent(SplitterResizerComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        hostFixture = TestBed.createComponent(TestHostComponent);
+        hostComponent = hostFixture.componentInstance;
+        hostFixture.detectChanges();
     });
 
     it("should create", () => {
-        expect(component).toBeTruthy();
+        expect(hostComponent).toBeTruthy();
     });
 });
