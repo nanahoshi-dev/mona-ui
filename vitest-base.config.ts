c59213884f2e3d42359860f5930c0105eae1906c
Phrolova <phrolova@nanahoshi.dev>
2026-08-25 06:14:50 +0300

    fix: possible fix for github runner memory issue

--- vitest-base.config.ts
+++ vitest-base.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        maxWorkers: 1,
+        isolate: true,
+        maxWorkers: 2,
         minWorkers: 1,
     },
 });
