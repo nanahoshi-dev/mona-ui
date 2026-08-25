b1f4310824f2bad979e9f8cee86a904d34118a36
Phrolova <phrolova@nanahoshi.dev>
2026-08-25 15:58:46 +0300

    chore: remove stray debug/scratch files from previous commit

--- eslint-summary.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("eslint-out.json", "utf8"));
-const files = data.filter(f => f.errorCount > 0);
-console.log("files with errors:", files.length);
-console.log("total errors:", files.reduce((a, f) => a + f.errorCount, 0));
-files.forEach(f => console.log(f.filePath));
